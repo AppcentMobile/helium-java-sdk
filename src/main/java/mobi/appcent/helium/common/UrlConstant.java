35c8a4fd26dc7a866f43fc730635d8dbad8d62d4
Eren Alpaslan <45710175+ErenAlpaslan@users.noreply.github.com>
2022-11-30 20:34:56 +0300

    #19 Adds rewards api (#39)
    Co-authored-by: Eren <eren.alparslan@appcent.mobi>

--- src/main/java/mobi/appcent/helium/common/UrlConstant.java
+++ src/main/java/mobi/appcent/helium/common/UrlConstant.java
@@ -7,6 +7,7 @@ public class UrlConstant {
     public static final String BASE_PATH = "https://api.helium.io/v1";
     public static final String PENDING_TRANSACTION_PATH = BASE_PATH + "/pending_transactions";
     public static final String ACCOUNTS_PATH = BASE_PATH + "/accounts";
+    public static final String REWARDS_PATH = BASE_PATH + "/rewards";
     public static final String OUIS_PATH = BASE_PATH + "/ouis";
     public static final String CHAIN_VARIABLES_PATH = BASE_PATH + "/vars";
     public static final String ORACLE_PRICES_PATH = BASE_PATH + "/oracle";
