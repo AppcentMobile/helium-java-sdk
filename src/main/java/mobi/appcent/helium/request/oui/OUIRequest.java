9e81345ff91e4d01984bee622a8208a0bf35ee34
Eren Alpaslan <45710175+ErenAlpaslan@users.noreply.github.com>
2022-11-30 20:32:06 +0300

    #18 Adds ouis api (#38)
    Co-authored-by: Eren <eren.alparslan@appcent.mobi>

---
+++ src/main/java/mobi/appcent/helium/request/oui/OUIRequest.java
@@ -0,0 +1,33 @@
+package mobi.appcent.helium.request.oui;
+
+import com.google.gson.reflect.TypeToken;
+import mobi.appcent.helium.HeliumSdkClient;
+import mobi.appcent.helium.common.UrlConstant;
+import mobi.appcent.helium.httpClient.HttpMethod;
+import mobi.appcent.helium.request.BaseRequest;
+import mobi.appcent.helium.response.ouis.OuisResponse;
+import okhttp3.Call;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collections;
+
+/**
+ * Created by erenalpaslan on 26.11.2022
+ */
+public class OUIRequest extends BaseRequest {
+    private final HeliumSdkClient client;
+    private final String oui;
+
+    public OUIRequest(HeliumSdkClient client, String oui) {
+        this.client = client;
+        this.oui = oui;
+    }
+
+    public OuisResponse execute() throws IOException {
+        String path = UrlConstant.OUIS_PATH + "/" + oui;
+        Call call = client.buildCall(path, HttpMethod.GET, Collections.emptyList(), null, null);
+        Type type = TypeToken.get(OuisResponse.class).getType();
+        return execute(call, type);
+    }
+}
