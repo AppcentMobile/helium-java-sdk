544f436a7006eaac5b29498070f3e7c9fd3bd282
Eren Alpaslan <45710175+ErenAlpaslan@users.noreply.github.com>
2022-12-12 10:53:58 +0300

    #31 Creates request classes for hotspot api (#51)
    Co-authored-by: Eren <eren.alparslan@appcent.mobi>

--- src/main/java/mobi/appcent/helium/request/hotspot/TotalRewardForHotspotRequest.java
+++ src/main/java/mobi/appcent/helium/request/hotspot/TotalRewardForHotspotRequest.java
@@ -1,19 +1,17 @@
-package mobi.appcent.helium.request.dc;
+package mobi.appcent.helium.request.hotspot;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import mobi.appcent.helium.HeliumSdkClient;
+import mobi.appcent.helium.api.HotspotApi;
 import mobi.appcent.helium.common.FieldConstant;
 import mobi.appcent.helium.common.UrlConstant;
 import mobi.appcent.helium.httpClient.HttpMethod;
 import mobi.appcent.helium.model.Pair;
 import mobi.appcent.helium.request.BaseRequest;
-import mobi.appcent.helium.response.account.AccountsResponse;
-import mobi.appcent.helium.response.dc.DCBurnTotalResponse;
-import mobi.appcent.helium.response.dc.DCBurnTotalsResponse;
+import mobi.appcent.helium.response.hotspot.HotspotTotalRewardResponse;
 import okhttp3.Call;
 import okhttp3.Response;
 
@@ -22,37 +20,42 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 /**
- * Created by erenalpaslan on 27.11.2022
+ * Created by erenalpaslan on 4.12.2022
  */
-public class DCBurnTotalsRequest extends BaseRequest {
+public class TotalRewardForHotspotRequest extends BaseRequest {
     private final HeliumSdkClient client;
-    private final String minTime;
+    private final String address;
     private String maxTime;
+    private String minTime;
     private String bucket;
 
-    public DCBurnTotalsRequest(HeliumSdkClient client, String minTime) {
+    public TotalRewardForHotspotRequest(HeliumSdkClient client, String address) {
         this.client = client;
-        this.minTime = minTime;
-    }
+        this.address = address; }
 
-    public DCBurnTotalsRequest maxTime(String maxTime) {
+    public TotalRewardForHotspotRequest maxTime(String maxTime) {
         this.maxTime = maxTime;
         return this;
     }
 
-    public DCBurnTotalsRequest bucket(String bucket) {
+    public TotalRewardForHotspotRequest minTime(String minTime) {
+        this.minTime = minTime;
+        return this;
+    }
+
+    public TotalRewardForHotspotRequest bucket(String bucket) {
         this.bucket = bucket;
         return this;
     }
 
-    public DCBurnTotalsResponse execute() throws IOException {
-        String path = UrlConstant.DC_BURNS_PATH + "/sum";
+    public HotspotTotalRewardResponse execute() throws IOException {
+        String path = UrlConstant.HOTSPOTS_PATH +"/"+address+"/rewards/sum";
         ArrayList<Pair> queryParams = new ArrayList<>();
         queryParams.add(Pair.create(FieldConstant.MIN_TIME, minTime));
         queryParams.add(Pair.create(FieldConstant.MAX_TIME, maxTime));
         queryParams.add(Pair.create(FieldConstant.BUCKET, bucket));
         Call call = client.buildCall(path, HttpMethod.GET, queryParams, null, null);
-        Type type = TypeToken.get(DCBurnTotalsResponse.class).getType();
+        Type type = TypeToken.get(HotspotTotalRewardResponse.class).getType();
         return execute(call, type);
     }
 
